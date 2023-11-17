af8b65fc884c3a01f9c375b19572335244cd48fd
ZiftK <alvaro.cab.lag@gmail.com>
2023-11-17 10:12:46 -0800

    add solve

--- pci.py
+++ pci.py
@@ -22,7 +22,31 @@ class PCI:
         self.__df = dfop.read_csv(data_path)
         self.__ddf = self.__df.copy()
 
+    def __solve(self, edf : dfop.DataFrame):
+        '''
+        Train system to interpolate data (get polynomial)
+
+        Returns
+        -------
+        None.
+
+        '''
         
+
+        # matrix to resolve
+        m = list()
+
+        # evaluate each x value into each matrix function line
+        for x in edf["x"]:
+            m.append(aop.valpow(  x,self.__exp))
+        
+        # ______ SOLVE ______
+        m = matrix(m)
+        
+        
+        # save coefficients
+        self.__coefficients = linalg.solve(m, array(edf["y"]))
+        self.__coefficients = round(self.__coefficients,self.__rounder)
 
 
 
