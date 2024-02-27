3722f8cc6d3e90633aa43897e02a66349b524d1b
ZiftK <alvaro.cab.lag@gmail.com>
2024-02-27 12:28:57 -0800

    add: split_by function

--- mathematics/dfop.py
+++ mathematics/dfop.py
@@ -1,52 +1,66 @@
-
 from pandas import read_csv, concat
 from pandas import DataFrame, Series
 
 from numpy import arange
 
 
-def min_value(df : DataFrame, column_name : str):
-    '''
+def split_by(df: DataFrame, columns: list) -> list[DataFrame]:
+    """
+    Split the DataFrame into sub DataFrames with equal values for
+    the corresponding columns and return a list with all the sub DataFrames
+    """
+    groups = df.groupby(columns)
+
+    return [group for _, group in groups]
+
+def min_value(df: DataFrame, column_name: str):
+    """
     Return min valuue from column in data frame
-    '''
+    """
 
     return df[column_name].min()
 
-def max_value(df: DataFrame, column_name : str):
+
+def max_value(df: DataFrame, column_name: str):
     '''
     Return max value from column in data frame
     '''
 
     return df[column_name].max()
 
-def get_val_from_index(df : DataFrame, column_name : str ,index : int):
+
+def get_val_from_index(df: DataFrame, column_name: str, index: int):
     '''
     Return index row value from passed column
     '''
 
     return df[column_name].iloc[index]
 
-def segment(df : DataFrame,initial_index : int, final_index : int) -> DataFrame:
+
+def segment(df: DataFrame, initial_index: int, final_index: int) -> DataFrame:
     '''
     Segment data frame from *initial_index* to *final_index* and return it
     '''
     return df[initial_index:final_index]
 
-def near_val(df : DataFrame, column_name : str, val):
+
+def near_val(df: DataFrame, column_name: str, val):
     '''
     Search nearest value to *val* in column
     '''
 
     return df['x'].iloc[(df[column_name] - val).abs().idxmin()]
 
-def get_index(df : DataFrame, column_name : str, val):
+
+def get_index(df: DataFrame, column_name: str, val):
     '''
     Return index of value *val* in column *column_name*
     '''
 
     return df.index[df[column_name] == val][0]
 
-def mean_diff(df : DataFrame, column_name : str)-> float:
+
+def mean_diff(df: DataFrame, column_name: str) -> float:
     '''
     Returns mean diff of data in column *column_name*
     '''
@@ -57,16 +71,17 @@ def mean_diff(df : DataFrame, column_name : str)-> float:
     # Calcula el promedio de las diferencias
     return diff.value_counts().idxmax()
 
-def insert(df: DataFrame,column_name : str, index : int, value : float):
+
+def insert(df: DataFrame, column_name: str, index: int, value: float):
     '''
     
     '''
 
-    
     df.loc[index:, column_name] = df.loc[index:, column_name].shift(1)
     df.loc[index, column_name] = value
 
-def soft_insert(df: DataFrame, row : dict, index : int)-> DataFrame:
+
+def soft_insert(df: DataFrame, row: dict, index: int) -> DataFrame:
     '''
     Adds a row with the specified index, shifting the replacement 
     row and all others to the right.
@@ -86,11 +101,11 @@ def soft_insert(df: DataFrame, row : dict, index : int)-> DataFrame:
 
     # The 'up' segment comprises from the beginning of
     # the DataFrame to the insertion position
-    up = segment(df,0,index)
+    up = segment(df, 0, index)
 
     # The 'up' segment comprises from the insertion
     # position to the end of the DataFrame
-    down = segment(df,index, len(df))
+    down = segment(df, index, len(df))
 
     # Since the 'ignore_index' option must be set to
     # 'True' to insert a row from a dictionary, 
@@ -105,43 +120,43 @@ def soft_insert(df: DataFrame, row : dict, index : int)-> DataFrame:
     new_row = Series(row)
 
     # We add the new row to the 'up' segment
-    new_df = up._append(new_row,ignore_index = True)
+    new_df = up._append(new_row, ignore_index=True)
     # Join all segments
     new_df = new_df._append(down)
 
     # Return join data frame
     return new_df
 
-def set_value(df : DataFrame, column_name : str, index : int, value : float):
+
+def set_value(df: DataFrame, column_name: str, index: int, value: float):
     '''
         Set value of specified column and index into value passed
     '''
     df.loc[index, column_name] = value
 
 
-
 class DataRange:
 
-    def __init__(self,df: DataFrame) -> None:
-        
-        self.__df = None # data frame
+    def __init__(self, df: DataFrame) -> None:
 
-        if type(df) == DataFrame: #try get data frame
+        self.__df = None  # data frame
+
+        if type(df) == DataFrame:  # try get data frame
             self.__df = df
         elif type(df) == str:
             self.__df = read_csv(df)
-        else:# if param type are not data frame or string raise exception
+        else:  # if param type are not data frame or string raise exception
             raise TypeError("A data frame or string path are expected")
-    
+
     @DeprecationWarning
     def insert(self, index, value, column_name):
         '''
         
         '''
 
-        insert(self.__df,column_name,index,value)
+        insert(self.__df, column_name, index, value)
 
-    def soft_insert(self, row : dict, index : int):
+    def soft_insert(self, row: dict, index: int):
         '''
         Adds a row with the specified index, shifting the replacement 
         row and all others to the right.
@@ -149,70 +164,69 @@ class DataRange:
         The specified value will be assigned to the specified column; 
         all other columns will have a default value.
         '''
-        self.__df = soft_insert(self.__df,row,index)
+        self.__df = soft_insert(self.__df, row, index)
 
-    def extract_df(self, initial_index : int, final_index : int):
+    def extract_df(self, initial_index: int, final_index: int):
         '''
         Return a sub data frame from original data frame, from initial to final index
         '''
-        return segment(self.__df,initial_index,final_index)
-    
-    def min_val_from(self, column_name : str):
+        return segment(self.__df, initial_index, final_index)
+
+    def min_val_from(self, column_name: str):
         '''
         Return min value from specifyc column
         '''
 
-        return min_value(self.__df,column_name)
+        return min_value(self.__df, column_name)
 
-    def max_val_from(self, column_name : str):
+    def max_val_from(self, column_name: str):
         '''
         Return max value from specfyc column
         '''
-        
-        return max_value(self.__df,column_name)
-    
-    def is_inside(self, value, column_name : str):
+
+        return max_value(self.__df, column_name)
+
+    def is_inside(self, value, column_name: str):
         '''
         Check if value is inside data range of specifyc column
         '''
         return value >= self.min_val_from(column_name) and value <= self.max_val_from(column_name)
-    
-    def is_in_column(self,value, column_name : str):
+
+    def is_in_column(self, value, column_name: str):
         '''
         Check if passed value is in passed column
         '''
         return value in self.__df[column_name].values
-    
-    def get_mean_diff(self, column_name : str):
+
+    def get_mean_diff(self, column_name: str):
         '''
         Return the mean diff of data in data frame from specyfic column
         '''
 
         return mean_diff(self.__df, column_name)
-    
-    def get_value(self, column_name : str, index : int):
+
+    def get_value(self, column_name: str, index: int):
         '''
         Return index value from column name
         '''
 
-        return get_val_from_index(self.__df,column_name,index)
-    
-    def set_value(self,column_name :str, index, value):
+        return get_val_from_index(self.__df, column_name, index)
+
+    def set_value(self, column_name: str, index, value):
         '''
         Set value of specified column and index into value passed
         '''
 
-        set_value(self.__df,column_name,index, value)
+        set_value(self.__df, column_name, index, value)
 
-    
-    def get_near_value(self, column_name : str, val):
+    def get_near_value(self, column_name: str, val):
         '''
         Search nearest value to *val* in column and return it
         '''
-            
+
         return near_val(self.__df, column_name, val)
-    
-    def sort_by(self, column_name : str):
+
+    def sort_by(self, column_name: str):
         '''
         Sort original data frame using values from passed column
         '''
@@ -224,11 +238,9 @@ class DataRange:
         Return rows count
         '''
         return len(self.__df)
-    
-    def get_index(self,column_name : str, val):
+
+    def get_index(self, column_name: str, val):
         '''
         Return first match index of value passed
         '''
-        return get_index(self.__df,column_name,val)
-    
-    
+        return get_index(self.__df, column_name, val)
